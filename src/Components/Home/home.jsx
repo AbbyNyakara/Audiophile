3ee9bbf3b6c0e3ddf50cc1d5967043e56c8eb887
Abigael Nyakara <mogusuab@msu.edu>
2024-05-16 14:06:55 -0400

    home page setup

---
+++ src/Components/Home/home.jsx
@@ -0,0 +1,10 @@
+// https://audiophilekdg.netlify.app/
+import './home.sass'
+
+const Home = () => {
+  return (
+    <div>home</div>
+  )
+}
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDom from 'react-dom'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App/>
+  </React.StrictMode>
 )
