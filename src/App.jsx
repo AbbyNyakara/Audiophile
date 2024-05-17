4b1955bd3a8573e8aaa05fac9724b78578a0d55c
Abigael Nyakara <mogusuab@msu.edu>
2024-05-17 13:02:26 -0400

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Home from "./Components/Home/home";
-
+import './index.scss';
 
 const App = () => {
   return(
-    <div>
+    <div className="">
       <Home />
     </div>
   )
--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,9 +1,15 @@
 // https://audiophilekdg.netlify.app/
-import './home.sass'
+import './home.scss'
 
 const Home = () => {
   return (
-    <div>home</div>
+    <div className='home_background'>
+      <nav className='top navbar_top'>
+       <img src="/images/shared/tablet/icon-hamburger.svg" alt="" />
+       <img src="/images/shared/desktop/logo.svg" alt="" />
+       <img src="/images/shared/desktop/icon-cart.svg" alt="" />
+      </nav>
+    </div>
   )
 }
 
