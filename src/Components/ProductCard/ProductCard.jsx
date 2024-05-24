97ba3d011f20e4c6a95d13fd306550acd88d5992
Abigael Nyakara <mogusuab@msu.edu>
2024-05-23 21:22:22 -0400

    add new

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -3,15 +3,16 @@ import './productcard.scss';
 
 ProductCard.PropTypes = {
   image: PropTypes.string,
-  product: PropTypes.string
+  product: PropTypes.string,
+  link: PropTypes.string
 }
-
 
 const ProductCard = () => {
   return (
     <div>
       <img src={this.props.image} alt="" />
       <h2>{this.props.product}</h2>
+      <a href={this.props.link}>Link</a>
     </div>
   )
 }
